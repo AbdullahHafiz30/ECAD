3d514d9df01b014116a24f333f42ae080eb67852
Mohammed Alamri <tu4102505@taibahu.edu.sa>
2024-03-04 03:11:52 +0300

    .

--- DataBase.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "ecad";
-
-$conn =  mysqli_connect($servername, $username, $password, $dbname);
-
-if(!$conn) {echo 'Error:' . mysqli_connect_error();}
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(isset($_POST['Logout'])){
-unset($Consumer_ID);
-unset($Admin_ID);
-header('Refresh:1; url=index.php');
-}
-?>
