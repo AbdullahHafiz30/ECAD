f923b39a0705ca55b8fd5a8a62145aafb157464f
Yosef590 <129640822+Yosef590@users.noreply.github.com>
2024-03-22 08:39:10 +0300

    .

--- admin/admin_alerts.php
+++ admin/admin_alerts.php
@@ -5,26 +5,47 @@ include '../side-bar.php';
 include '../DB/DataBase.php';
 
 header("refresh: 5");
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Check if a checkbox is selected
+    if (isset($_POST['delete'])) {
+        foreach ($_POST['delete'] as $deleteId) {
+            $deleteId = mysqli_real_escape_string($conn, $deleteId);
+            $deleteQuery = "DELETE FROM `anomaly` WHERE `Anomaly_ID` = '$deleteId'";
+            mysqli_query($conn, $deleteQuery);
+        }
+        // Redirect to refresh the page
+        header("Location: ".$_SERVER['PHP_SELF']);
+        exit();
+    }
+}
+
 $queryAnomaly = "SELECT * FROM `anomaly`";
 $result = mysqli_query($conn, $queryAnomaly);
 
 $conn->close();
-
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/alerts.css">
+    <link rel="stylesheet" href="./Style.css">
     <title>Alerts page</title>
+    <script>
+        function deleteRows() {
+            document.getElementById('deleteForm').submit();
+        }
+    </script>
 </head>
 
 <body>
-    <section class="home">
-        <div class="text">Alerts</div>
-        <div class="center-table">
+    <div class="center-table">
+        <h1>Alerts</h1><br><br>
+        <form id="deleteForm" method="POST">
             <table class="content-table">
                 <thead>
                     <tr>
@@ -32,38 +53,27 @@ $conn->close();
                         <th scope="col">Timestamp </th>
                         <th scope="col">Energy Unit Number </th>
                         <th scope="col">Consumption Value </th>
+                        <th scope="col">Done</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <?php
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            ?>
-
-                            <td>
-                                <?php echo $row['Anomaly_ID'] ?>
-                            </td>
-                            <td>
-                                <?php echo $row['Timestamp'] ?>
-                            </td>
-                            <td>
-                                <?php echo $row['Energy_UnitNumber'] ?>
-                            </td>
-                            <td>
-                                <?php echo $row['Consumptions_value'] ?>
-                            </td>
-
-
+                    <?php
+                    while ($row = mysqli_fetch_assoc($result)) {
+                    ?>
+                        <tr>
+                            <td><?php echo $row['Anomaly_ID'] ?></td>
+                            <td><?php echo $row['Timestamp'] ?></td>
+                            <td><?php echo $row['Energy_UnitNumber'] ?></td>
+                            <td><?php echo $row['Consumptions_value'] ?></td>
+                            <td><input type="checkbox" name="delete[]" value="<?php echo $row['Anomaly_ID'] ?>" onclick="deleteRows()"></td>
                         </tr>
-                        <?php
-                        }
-
-                        ?>
+                    <?php
+                    }
+                    ?>
                 </tbody>
             </table>
-        </div>
-    </section>
-
+        </form>
+    </div>
 </body>
 
 </html>
